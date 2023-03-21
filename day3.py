01c8beea64619f435ce2ef60c43c65b8d99b80d3
busra2 <115122063+busra2@users.noreply.github.com>
2023-03-21 21:29:13 +0300

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,22 @@
+class Human:
+    #name = "büşra"
+    def __init__(self,name):
+       # self.name=name diyemem not defined 
+       self.name = name
+    def talk(self,sentence):
+        print(f"{self.name}: {sentence}")
+    def walk (self):
+        print(f"{self.name} is walking")
+
+# instance => örnek
+human1 = Human("büş")
+#human1.name="esma"
+human1.talk("Merhaba")
+human1.walk()
+print(human1)
+
+human2 = Human("halit")
+#human2.name="Esma"
+human2.talk("Selam")
+human2.walk()
+
